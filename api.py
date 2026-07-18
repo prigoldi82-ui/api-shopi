9bf814ca4134b42139fa33e8e62d54a0c916cd21
prigoldi82-ui <prigoldi82@gmail.com>
2026-07-18 17:53:31 +0530

    Update api.py

--- api.py
+++ api.py
@@ -1075,4 +1075,8 @@ def shopify_checker():
         }), 500
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8056, debug=False)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 8081)),
+        debug=False
+    )
